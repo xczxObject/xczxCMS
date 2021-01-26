d229d731622fef1502d056d5f0fbb1dc0fd20479
147258369asdjkl <2876721452@qq.com>
2021-01-26 14:44:34 +0800

    2021/1/26号提交_p

---
+++ xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -0,0 +1,9 @@
+package com.xuecheng.govern.center;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer//标识此工程是一个EurekaServer
+@SpringBootApplication
+public class GovernCenterApplication {
+}
